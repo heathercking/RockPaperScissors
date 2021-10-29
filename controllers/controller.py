e1f91acb44d928ffa2bf2aa67890d48fd5977cf1
Heather King <hkingdevelopment@gmail.com>
2021-10-29 19:59:09 +0100

    play form created
    form created on /play route, currently does nothing

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,7 +7,15 @@ from models.player import Player
 def index():
     return render_template('index.html', title='Home')
 
-@app.route('/<player1_choice>/<player2_choice>')
+@app.route('/rules')
+def rules():
+    return render_template('rules.html', title='Rules')
+
+@app.route('/play')
+def play():
+    return render_template('play.html', title='Play')
+
+@app.route('/result/<player1_choice>/<player2_choice>')
 def play_game(player1_choice, player2_choice):
     game1 = Game("Rock Paper Scissors")
     player1 = Player("Player 1")
@@ -15,8 +23,4 @@ def play_game(player1_choice, player2_choice):
     player2 = Player("Player 2")
     player2.assign_choice_to_player(player2_choice)
     result = game1.determine_winner(player1, player2)
-    return render_template('play.html', title='Play', p1 = player1, p2 = player2, result = result)
-
-@app.route('/rules')
-def rules():
-    return render_template('rules.html', title='Rules')
+    return render_template('result.html', title='Result', p1 = player1, p2 = player2, result = result)
