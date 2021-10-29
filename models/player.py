350fd63c8246d49d9778e9a8d30470d39660b7f5
Heather King <hkingdevelopment@gmail.com>
2021-10-29 12:16:53 +0100

    player class setup complete
    adds method to assign choice to player, tests player has a name and that choice can be assigned to them

--- models/player.py
+++ models/player.py
@@ -3,3 +3,6 @@ class Player():
     def __init__(self, name):
         self.name = name
         self.choice = ""
+
+    def assign_choice_to_player(self, input_choice):
+        self.choice = input_choice
--- tests/player_test.py
+++ tests/player_test.py
@@ -7,5 +7,11 @@ class TestPlayer(unittest.TestCase):
         self.player1 = Player("Smithy")
         self.player2 = Player("Kingy")
 
-    
+    def test_player_has_name(self):
+        self.assertEqual("Smithy", self.player1.name)
+
+    def test_assign_choice_to_player(self):
+        self.player1.assign_choice_to_player("rock")
+        self.assertEqual("rock", self.player1.choice)
+
 
