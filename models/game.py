ad974582e48b78e37cb259ddfa9e2cbcc2c0da7b
Heather King <hkingdevelopment@gmail.com>
2021-10-29 13:08:03 +0100

    tests written for determine_winner function

--- models/game.py
+++ models/game.py
@@ -3,6 +3,6 @@ class Game():
     def __init__(self, name):
         self.name = name
     
-    def determine_winner(self, player1, player2):
-            
+    # def determine_winner(self, player1, player2):
+
 
